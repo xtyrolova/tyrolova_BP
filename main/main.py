4f9ec7f9b11d8614737a8f93173e7281d5b43758
xtyrolova <xtyrolova@stuba.sk>
2021-10-03 22:49:35 +0200

    average color

--- main/main.py
+++ main/main.py
@@ -1,20 +1,42 @@
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
 #import argparse
-def load_image():
+def load_image(image):
 
     #ap = argparse.ArgumentParser()
     #ap.add_argument("-i", "--image", required = True, help = "Path to the image")
     #args = vars(ap.parse_args())
 
-    image = cv2.imread("C:/Users/tyrol_6l57e8e/BP_tyrolova/main/fei.jpeg")
-
-    scale_percent = 50 # percent of original size
-    width = int(image.shape[1] * scale_percent / 100)
-    height = int(image.shape[0] * scale_percent / 100)
-    dim = (width, height)
-
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    dim = (500, 300)
     image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
-    cv2.imshow("FEI STU '21", image)
-    cv2.waitKey(0)
 
-load_image()
+def show_img_comp(image1, image2):
+    f, ax = plt.subplots(1, 2, figsize=(10,5))
+    ax[0].imshow(image1)
+    ax[1].imshow(image2)
+    ax[0].axis('off') #hide the axis
+    ax[1].axis('off')
+    f.tight_layout()
+    plt.show()
+
+    load_image(image1)
+    load_image(image2)
+
+def average(image):
+    img_temp = image.copy()
+    img_temp[:,:,0], img_temp[:,:,1], img_temp[:,:,2] = np.average(image, axis=(0,1))
+    return img_temp
+
+def main():
+    image1 = cv2.imread("C:/Users/tyrol_6l57e8e/BP_tyrolova/main/fei.jpeg")
+    image2 = cv2.imread("C:/Users/tyrol_6l57e8e/BP_tyrolova/main/fiit.jpg")
+    av_color_im1 = average(image1)
+    av_color_im2 = average(image2)
+
+    show_img_comp(image1, av_color_im1)
+    show_img_comp(image2, av_color_im2)
+
+main()
