83c9bcf2cf6ea59f4122fa41a320e3b45836656e
xtyrolova <xtyrolova@stuba.sk>
2021-10-20 23:25:52 +0200

    read+make+plot SSIM

---
+++ graphs/MakeSsim.py
@@ -0,0 +1,36 @@
+from skimage.metrics import structural_similarity as ssim
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import csv
+
+# OPEN AND LOAD DATASET
+f = open('dtb.csv', 'r')
+file = csv.DictReader(f, delimiter=';')
+
+ids = []
+images1 = []
+images2 = []
+values = []
+
+for col in file:
+    col.keys()
+    ids.append(col['ID'])
+    images1.append(col['path1'])
+    images2.append(col['path2'])
+    values.append(col['value'])
+
+# CREATE AND MAKE MSE
+header = ['ID', 'value', 'SSIM']
+
+fileMSE = open('ssim.csv', 'w+', newline='')
+writer = csv.writer(fileMSE, delimiter=';')
+writer.writerow(header)
+
+for i in range(0, len(ids)):
+    image1 = cv2.imread(images1[i])
+    image2 = cv2.imread(images2[i])
+    image1 = cv2.resize(image1, (1206,1820))
+    image2 = cv2.resize(image2, (1206,1820))
+    row = [ids[i], values[i], ssim(image1, image2, multichannel=True)]
+    writer.writerow(row)
--- graphs/makeGraph.py
+++ graphs/makeGraph.py
@@ -9,9 +9,19 @@ import pandas as pd
 #         self.second = second
 #         self.value = value
 
-df = pd.read_csv('mse.csv', delimiter=';')
-sns.histplot(data=df, x='ID', y='MSE', bins=20, color="pink")
+fig, axes = plt.subplots(1, 2, figsize=(10,5))
+fig.suptitle('MSE vs SSIM')
 
-plt.xlabel('ID')
-plt.ylabel('MSE')
+dataMSE = pd.read_csv('mse.csv', delimiter=';')
+sns.histplot(data=dataMSE, x='ID', y='MSE', bins=20, color="pink", ax=axes[0])
+
+# plt.xlabel('ID')
+# plt.ylabel('MSE')
+# plt.show()
+dataSSIM = pd.read_csv('ssim.csv', delimiter=';')
+sns.histplot(data=dataSSIM, x='ID', y='SSIM', bins=20, color="green", ax=axes[1])
+
+# plt.xlabel('ID')
+# plt.ylabel('SSIM')
 plt.show()
+
